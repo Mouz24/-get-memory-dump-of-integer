8718eb911968a9fbd0e21d074c7a48e010945ff1
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-08-29 15:49:18 +0300

    Done

---
+++ BinaryRepresentation/BitsManipulation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace BinaryRepresentation
+{
+    public static class BitsManipulation
+    {
+        /// <summary>
+        /// Get binary memory representation of signed long integer.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>Binary memory representation of signed long integer.</returns>
+        public static string GetMemoryDumpOf(long number)
+        {
+            var sb = new StringBuilder();
+            int length = 0;
+            if (number < 0)
+            {
+                return Convert.ToString(number, 2);
+            }
+            else
+            {
+                while (length != 64)
+                {
+                    if ((number & 1) == 1)
+                    {
+                        sb.Insert(0, '1');
+                        length++;
+                    }
+                    else
+                    {
+                        sb.Insert(0, '0');
+                        length++;
+                    }
+
+                    number >>= 1;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
